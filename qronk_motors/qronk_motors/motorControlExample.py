5e334b63bc60a0c10ead26ca9f14d2ab16c51834
Jin Rhee <jinrhee2003@gmail.com>
2024-01-28 23:31:29 +0900

    merge conflict on jin's rpi

--- qronk_motors/qronk_motors/motorControlExample.py
+++ qronk_motors/qronk_motors/motorControlExample.py
@@ -5,8 +5,8 @@ from time import sleep   # Imports sleep (aka wait or pause) into the program
 GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
 
 # Set up pin 16 for PWM
-GPIO.setup(16,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
-p = GPIO.PWM(16, 50)     # Sets up pin 11 as a PWM pin
+GPIO.setup(3,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
+p = GPIO.PWM(3, 50)     # Sets up pin 11 as a PWM pin
 p.start(0)               # Starts running PWM on the pin and sets it to 0
 
 # Move the servo back and forth
