dc548b3b61e21c40d197412c947802f337d9a795
Jin Rhee <jinrhee2003@gmail.com>
2024-02-10 11:03:36 +0900

    Change to cont. servos, started work on throttle function

--- qronk_motors/qronk_motors/state2servo.py
+++ qronk_motors/qronk_motors/state2servo.py
@@ -16,22 +16,46 @@ class JointSub(Node):
 
     def __init__(self,nServos):
         super().__init__('JointSub')
-        self.subscription = self.create_subscription(
-            JointState,
-            'joint_states',
-            self.listener_callback,
-            10) #Sub to the joint state message
-        self.subscription  # prevent unused variable warning
-        self.i2c = busio.I2C(SCL, SDA) #connect to driver board
-        self.pca = PCA9685(self.i2c) 
-        self.pca.frequency = 50
-        self.servos = [servo.Servo(self.pca.channels[i]) for i in range(nServos)] #Initialize servos
+        
+        # Subscribe to joint_states topic
+        self.create_subscription(JointState,'joint_states',self.listener_callback,10)
+        
+        _i2c = busio.I2C(SCL, SDA) # Connect to i2c bus
+        self.pca = PCA9685(_i2c) # Create PCA9685 instance
+
+        self.pca.frequency = 50 # Set PCA frequncy to 50Hz
+        self.topic_freq = 30 # Hz
+        self.max_speed = 50 * 2 * pi # Angular vel. when servo is fully throttled (~ 50rpm, should be tested)
+        
+        # Initialize continuous servos
+        self.servos = [servo.ContinuousServo(self.pca.channels[i],
+                                             min_pulse = 1300, # 
+                                             max_pulse = 1700) for i in range(nServos)]
 
     def listener_callback(self, msg):
-        self.pos = msg.position #Store names and positions
-        self.name = msg.name
+        # Unpack msg data
+        _timestamp = msg.header # Note header contains timestamp when joint angle was calculated
+        _names = msg.name # TODO: Agree on servo naming convention (I think simple 1, 2,3 or servo1, servo2, servo3 would be good)
+        _positions = msg.position
+        _vels = msg.velocity # TODO: Agree on units
+        _efforts = msg.effort
+
+        self.pos_old = self.pos # Store previous timestep position
+        self.pos = msg.pos # Store new timestep position
+        self.name = msg.name # Store name
+
+    def throttleServo(self, servoID: int = 0):
+        # TODO: Agree on topic puiblishing frequency and make sure publishers stick to it
+        _speed_ratio = 100 / self.max_speed # TODO: 100 here is placeholder
+        self.servos[servoID].throttle(_speed_ratio)
+        pass
+
+    def throttleServos(self, servos):
+        pass
 
     def positionServo(self,nServos):
+        pass
+        '''
         for i in range(nServos):
             p = self.pos[i]
             s = self.servos[i]
@@ -41,6 +65,7 @@ class JointSub(Node):
                 s.angle = -1
             else:
                 s.angle = p*(180/pi) #Set servo angle for all values inbetween
+        '''
     
     def closeBus(self): #Close the bus when finished
         self.pca.deinit()
